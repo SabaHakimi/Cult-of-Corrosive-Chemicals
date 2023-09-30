09c5ce2ccec0f086359d4264112461c9f69b7858
SabaHakimi <hakimisabo@gmail.com>
2023-09-29 18:09:23 -0700

    Barrels complete

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -19,27 +19,37 @@ class Barrel(BaseModel):
 
     quantity: int
 
+# Receive list of purchased barrels and finalize transaction
 @router.post("/deliver")
 def post_deliver_barrels(barrels_delivered: list[Barrel]):
-    """ """
-    print(barrels_delivered)
+    qry_sql = """SELECT num_red_ml, gold FROM global_inventory"""    
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(qry_sql))
+    data = result.first()
+
+    set_sql = """UPDATE global_inventory SET num_red_ml = {}, 
+    gold = {}""".format(data.num_red_ml + barrels_delivered[0].ml_per_barrel, 
+                            data.gold - barrels_delivered[0].price)
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(set_sql))
 
     return "OK"
 
 # Gets called once a day
+# Place order
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    """ """
+    sql_to_execute= """SELECT num_red_potions FROM global_inventory"""
     print(wholesale_catalog)
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory"))
-    
+        result = connection.execute(sqlalchemy.text(sql_to_execute))
     data = result.first()
     
-    if data.num_red_potions < 10:
-        return [
-            {
-                "sku": "SMALL_RED_BARREL",
-                "quantity": 1 if data.num_red_potions < 10 else 0,
-            }
-        ]
+    return [
+        {
+            "sku": "SMALL_RED_BARREL",
+            "quantity": 1 if data.num_red_potions < 10 and 
+            wholesale_catalog[0].price < data.gold and 
+            wholesale_catalog[0].quantity > 0 else 0,
+        }
+    ]
