75f41a379934c3745752fcdf5100b39ed75b92e8
SabaHakimi <hakimisabo@gmail.com>
2023-11-12 16:59:32 -0800

    no more barrel

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -75,6 +75,8 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 # Place order
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
+    return []
+    
     # Initialize Variables
     large_validation_set = {"LARGE_DARK_BARREL"}
     medium_validation_set = {"MEDIUM_DARK_BARREL"}
